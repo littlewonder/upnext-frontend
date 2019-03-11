ccbb6148ba80d681ae4760acfcc2304f65877c0b
Himanshu Shekhar <himanshushekharb16@gmail.com>
2019-03-11 17:14:27 +0530

    implement upvote/downvote on queue

--- src/components/Queue.vue
+++ src/components/Queue.vue
@@ -5,9 +5,11 @@
     <div class="songlist" v-if="queue.length > 0">
       <div class="song" v-for="(link, i) in queue" v-bind:key="i">
         <div class="upvotes">
-          <img @click="upvote(link, i)" src="../assets/upvote.svg" class="upvote">
+          <img @click="upvote(link, i)" src="../assets/upvote-done.svg" class="upvote" v-if="link.my_vote==1">
+          <img @click="upvote(link, i)" src="../assets/upvote.svg" class="upvote" v-else>
           <p>{{link.total_votes}}</p>
-          <img @click="downvote(link, i)" src="../assets/downvote.svg" class="downvote">
+          <img @click="downvote(link, i)" src="../assets/downvote-done.svg" class="downvote" v-if="link.my_vote==-1">
+          <img @click="downvote(link, i)" src="../assets/downvote.svg" class="downvote" v-else>
         </div>
         <div class="details">
           <h3>{{link.title}}</h3>
@@ -27,18 +29,21 @@ export default {
     return {
       queue: [],
       apiPollInterval: null,
+      apiLocked: false,
     }
   },
   created() {
     const self = this
     this.apiPollInterval = setInterval(async() => {
-
+      
+      if (self.apiLocked) return;
+      
       let resp = await api.upNextSongs()
       for (let i = 0; i < resp.links.length; i++) {
         let lid = String(resp.links[i].link_id)
         resp.links[i]['my_vote'] = resp.votes[lid] || 0
       }
-
+      
       self.queue = resp.links
     }, 3000)
   },
@@ -47,17 +52,60 @@ export default {
   },
   methods: {
     async upvote(link, i) {
+      this.apiLocked = true
+      
       let response = await api.upvoteLink(link.link_id)
       if (!response.ok) {
         alert(`Upvote failed on ${link.link_id}`)
       }
+      let oldscore = this.queue[i].my_vote
+      let my_vote = 1
+      let total_votes = this.queue[i].total_votes
+      
+      if (oldscore == 1)
+        total_votes += 0
+      else if (oldscore == -1)
+        total_votes += 2
+      else
+        total_votes += 1
+      
+      console.log(this.queue[i].total_votes, total_votes, my_vote)
+      
+      Object.assign(this.queue[i], {total_votes: total_votes, my_vote: my_vote})
+      
+      this.apiLocked = false
     },
+    
     async downvote(link, i) {
+      this.apiLocked = true
+      
       let response = await api.downvoteLink(link.link_id)
       if (!response.ok) {
         alert(`Downvote failed on ${link.link_id}`)
       }
+      let oldscore = this.queue[i].my_vote
+      let my_vote = -1
+      let total_votes = this.queue[i].total_votes
+      
+      if (oldscore == -1)
+        total_votes += 0
+      else if (oldscore == 1)
+        total_votes += -2
+      else
+        total_votes += -1
+      
+      console.log(this.queue[i].total_votes, total_votes, my_vote)
+      Object.assign(this.queue[i], {total_votes: total_votes, my_vote: my_vote})
+
+      this.apiLocked = false
     }
   }
 }
 </script>
+
+
+<style>
+  .upvote, .downvote {
+  cursor: pointer;
+  }
+</style>
