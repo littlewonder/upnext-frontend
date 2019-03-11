1db024a6a704bf11a075e6b876e5e66c77f430f7
Himanshu Shekhar <himanshushekharb16@gmail.com>
2019-03-11 11:00:24 +0530

    integrate queue view with backend

--- src/components/Queue.vue
+++ src/components/Queue.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="queue">
     <h1>Up Next</h1>
-    <div class="songlist">
-      <div class="song" v-for="(item,i) in queue" v-bind:key="i">
+    <div class="songlist" v-if="queue.length > 0">
+      <div class="song" v-for="(link, i) in queue" v-bind:key="i">
         <div class="upvotes">
           <img src="../assets/upvote.svg" class="upvote">
-          <p>{{item.upvotes}}</p>
+          <p>{{link.total_votes}}</p>
           <img src="../assets/downvote.svg" class="downvote">
         </div>
         <div class="details">
-          <h3>{{item.song}}</h3>
-          <h4>{{item.channel}}</h4>
+          <h3>{{link.title}}</h3>
+          <h4>{{link.channel_name}}</h4>
         </div>
       </div>
     </div>
@@ -18,89 +18,33 @@
 </template>
 
 <script>
+import api from '../api'
+
 export default {
   name: "Queue",
   data() {
     return {
-      queue: [
-        {
-          song: "Slow Dancing in a Burning room",
-          channel: "John Mayer Vevo",
-          upvotes: 12,
-          to: "Vivek Sharma",
-          from: "Abhishek",
-          url: "https://www.youtube.com/watch?v=WrY4qQEBTHM",
-          ifPlayed: false
-        },
-        {
-          song: "BOHEMIA - Umeed (Official Music Video)",
-          channel: "Bohemia",
-          upvotes: 9,
-          to: "Vivek Sharma",
-          from: "Abhishek",
-          url: "https://www.youtube.com/watch?v=WrY4qQEBTHM",
-          ifPlayed: false
-        },
-        {
-          song: "Prateek Kuhad - Tum Jab Paas",
-          channel: "JioSaavn",
-          upvotes: 3,
-          to: "Vivek Sharma",
-          from: "Abhishek",
-          url: "https://www.youtube.com/watch?v=WrY4qQEBTHM",
-          ifPlayed: false
-        },
-        {
-          song: "Kyu (Prod. by Sez On The Beat) | Seedhe Maut ...",
-          channel: "Seedhe Maut",
-          upvotes: 5,
-          to: "Vivek Sharma",
-          from: "Abhishek",
-          url: "https://www.youtube.com/watch?v=WrY4qQEBTHM",
-          ifPlayed: false
-        },
-        {
-          song: "Pink Floyd - Comfortably Numb [HD]",
-          channel: "David Gilmour",
-          upvotes: 2,
-          to: "Vivek Sharma",
-          from: "Abhishek",
-          url: "https://www.youtube.com/watch?v=WrY4qQEBTHM",
-          ifPlayed: false
-        }
-      ],
-      submissions: [
-        {
-          song:
-          "Full Power (Yungsta x Frappe Ash) - NaNa (Prod. by Stunnah Beatz...",
-          channel: "Frappe Ash",
-          ifPlayed: true,
-          upvotes: 2,
-          to: "Vivek Sharma",
-          from: "Abhishek",
-          url: "https://www.youtube.com/watch?v=WrY4qQEBTHM"
-        },
-        {
-          song: "Bol Beta | Yungsta | Encore ABJ | Elements | 2018",
-          channel: "Yungsta (Full Power)",
-          ifPlayed: false,
-          upvotes: 2,
-          to: "Vivek Sharma",
-          from: "Abhishek",
-          url: "https://www.youtube.com/watch?v=WrY4qQEBTHM"
-        },
-        {
-          song:
-          "Full Power (Yungsta x Frappe Ash) - NaNa (Prod. by Stunnah Beatz...",
-          channel: "Frappe Ash",
-          ifPlayed: false,
-          upvotes: 2,
-          to: "Vivek Sharma",
-          from: "Abhishek",
-          url: "https://www.youtube.com/watch?v=WrY4qQEBTHM"
-        }
-      ]
+      queue: [],
+      apiPollInterval: null,
     }
+  },
+  created() {
+    const self = this
+    this.apiPollInterval = setInterval(async() => {
+      let resp = await api.upNextSongs()
+      for (let i = 0; i < resp.links.length; i++) {
+        let lid = String(resp.links[i].link_id)
+        if (resp.votes[lid])
+          resp.links[i]['my_vote'] = resp.votes[lid]
+        else
+          resp.links[i]['my_vote'] = 0
+      }
+
+      self.queue = resp.links
+    }, 1000)
+  },
+  beforeDestroy() {
+    clearInterval(this.apiPollInterval)
   }
 }
 </script>
